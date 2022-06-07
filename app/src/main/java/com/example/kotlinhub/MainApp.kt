57f1c4982da9343a42b0625829375082d20827d6
Aaditya <aady.prakash@gmail.com>
2022-06-07 20:47:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/kotlinhub/MainApp.kt
@@ -0,0 +1,17 @@
+package com.example.kotlinhub
+
+import android.app.Application
+
+class MainApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initApp();
+    }
+
+    private fun initApp() {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/com/example/kotlinhub/fragments/HomeFragment.kt
@@ -0,0 +1,4 @@
+package com.example.kotlinhub.fragments
+
+class HomeFragment {
+}
